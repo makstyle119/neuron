632709e744cf2a2a4958be1c9449418a3582eb04
Mohammad Moiz Ali <moizprince786@gmail.com>
2025-12-14 16:56:19 +0500

    some testing

--- app.py
+++ app.py
@@ -2,33 +2,23 @@ from neuron_network import NeuralNetwork
 
 # 2 input neurons → 3 hidden neurons → 1 output neuron
 layer_config = [
-    { 
-        "num_of_neurons": 2, 
-        "num_inputs_per_neuron": 2
-    }, 
-    { 
-        "num_of_neurons": 3, 
-        "num_inputs_per_neuron": 2
-    }, 
-    { 
-        "num_of_neurons": 1, 
-        "num_inputs_per_neuron": 3
-    }
+    {"num_of_neurons": 4, "num_inputs_per_neuron": 2},  # Hidden layer
+    {"num_of_neurons": 6, "num_inputs_per_neuron": 4}   # Output layer (multi-task)
 ]
 
 nn = NeuralNetwork(layer_config)
 
 # ----- XOR Training -----
 if __name__ == "__main__":
-    xor_data = [
-        ([0, 0], [0]),
-        ([0, 1], [1]),
-        ([1, 0], [1]),
-        ([1, 1], [0]),
+    logic_data = [
+        ([0, 0], [0, 0, 1, 1, 1, 1]),
+        ([0, 1], [0, 1, 1, 1, 0, 0]),
+        ([1, 0], [0, 1, 0, 1, 0, 0]),
+        ([1, 1], [1, 1, 0, 0, 0, 1])
     ]
 
     nn.train(
-        xor_data,
+        logic_data,
         epochs=100000,
         learning_rate=0.1,
         patience=3000,       # Wait max 3000 epochs for improvement
@@ -36,7 +26,8 @@ if __name__ == "__main__":
     )
 
 
-    print("\n--- Testing XOR ---")
-    for inputs, expected in xor_data:
+    print("\n--- Testing ---")
+    for inputs, expected in logic_data:
         prediction = nn.predict(inputs)
-        print(f"Input: {inputs} => Predicted: {prediction[0]:.4f}, Expected: {expected[0]}")
+        print(f"Input: {inputs} => Predicted: {prediction}, Expected: {expected}")
+        # print(f"Input: {inputs} => Predicted: {prediction[0]:.4f}, Expected: {expected[0]}")
